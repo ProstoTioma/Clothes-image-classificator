3d4ebf6da2deacd498167eef882b4cfc6eb5ed98
Artem Yermolenko <artemyermolenko14@gmail.com>
2021-02-06 14:02:41 +0100

    second commit

---
+++ temp.py
@@ -0,0 +1,54 @@
+from tensorflow.keras.datasets import fashion_mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import utils
+import numpy as np
+
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+
+x_train = x_train.reshape(60000, 784)
+x_train = x_train / 255 
+
+x_test = x_test.reshape(10000, 784)
+x_test = x_test / 255 
+
+
+y_train = utils.to_categorical(y_train, 10)
+
+y_test = utils.to_categorical(y_test, 10)
+
+classes = ['футболка', 'брюки', 'свитер', 'платье', 'пальто', 'туфли', 'рубашка',
+           'кроссовки', 'сумка', 'ботинки']
+
+model = Sequential()
+
+model.add(Dense(30, input_dim=784, activation="relu"))
+
+model.add(Dense(20, activation="relu"))
+
+model.add(Dense(10, activation="softmax"))
+
+model.compile(loss="categorical_crossentropy", optimizer="SGD", metrics=["accuracy"])
+
+print(model.summary())
+
+model.fit(x_train, y_train,
+          batch_size=200,
+          epochs=25)
+
+predictions = model.predict(x_test)
+
+i = 0
+count = 0
+while i < 10000:
+    if (np.argmax(predictions[i])) == np.argmax(y_test[i]):
+        count += 1
+    i += 1
+print("Правильных ответов:", count / 10000 * 100, "%")
+
+
+#print(predictions[0])
+
+#print(classes[np.argmax(predictions[0])])
+
+#print(classes[np.argmax(y_test[0])])
