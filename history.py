31d9dab5292b61ac8db494003fc8b4d83ed4b468
Artem Yermolenko <artemyermolenko14@gmail.com>
2021-02-06 14:04:29 +0100

    check

---
+++ history.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Sun Jan 17 12:42:55 2021)---
+runfile('C:/Users/User/.spyder-py3/temp.py', wdir='C:/Users/User/.spyder-py3')
+
+## ---(Sun Jan 17 12:48:24 2021)---
+runfile('C:/Users/User/.spyder-py3/temp.py', wdir='C:/Users/User/.spyder-py3')
+
+## ---(Sun Jan 17 14:21:40 2021)---
+runfile('C:/Users/User/.spyder-py3/temp.py', wdir='C:/Users/User/.spyder-py3')
+
+## ---(Sun Jan 17 15:17:50 2021)---
+runfile('C:/Users/User/.spyder-py3/temp.py', wdir='C:/Users/User/.spyder-py3')
+
+## ---(Sun Jan 17 18:24:42 2021)---
+runfile('C:/Users/User/.spyder-py3/temp.py', wdir='C:/Users/User/.spyder-py3')
+
+## ---(Mon Jan 18 09:20:24 2021)---
+runfile('C:/Users/User/.spyder-py3/temp.py', wdir='C:/Users/User/.spyder-py3')
+
+## ---(Mon Jan 18 09:28:47 2021)---
+runfile('C:/Users/User/.spyder-py3/temp.py', wdir='C:/Users/User/.spyder-py3')
+
+## ---(Thu Jan 21 08:47:18 2021)---
+runfile('C:/Users/User/.spyder-py3/temp.py', wdir='C:/Users/User/.spyder-py3')
+7
+runfile('C:/Users/User/.spyder-py3/temp.py', wdir='C:/Users/User/.spyder-py3')
+
+## ---(Mon Jan 25 09:33:54 2021)---
+runfile('C:/Users/User/.spyder-py3/temp.py', wdir='C:/Users/User/.spyder-py3')
+
+## ---(Thu Jan 28 10:41:04 2021)---
+runfile('C:/Users/User/.spyder-py3/temp.py', wdir='C:/Users/User/.spyder-py3')
+
+## ---(Tue Feb  2 12:44:30 2021)---
+runfile('C:/Users/User/.spyder-py3/temp.py', wdir='C:/Users/User/.spyder-py3')
+
+## ---(Wed Feb  3 09:19:31 2021)---
+runfile('C:/Users/User/.spyder-py3/temp.py', wdir='C:/Users/User/.spyder-py3')
+
+## ---(Thu Feb  4 09:54:52 2021)---
+runfile('C:/Users/User/.spyder-py3/temp.py', wdir='C:/Users/User/.spyder-py3')
+runfile('C:/Users/User/.spyder-py3/untitled0.py', wdir='C:/Users/User/.spyder-py3')
+runfile('C:/Users/User/.spyder-py3/temp.py', wdir='C:/Users/User/.spyder-py3')
+
+## ---(Sat Feb  6 10:58:09 2021)---
+runfile('C:/Users/User/untitled0.py', wdir='C:/Users/User')
